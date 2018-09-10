b78eb8bc630f0aa4a9479a2f9861561525e13085
Damien Siri <dsiri12@gmail.com>
2018-09-09 22:03:23 -0400

    added date in response

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const http = require('http');
 const os = require('os');
 
-console.log("Ying's web server starting...");
+console.log("Frank's web server starting...");
 
 var handler = function(request, response) {  
     console.log("Received request from " + request.connection.remoteAddress);
       response.writeHead(200);  
-      response.end("Frank, You've hit " + os.hostname() + "\n");
+      response.end("Hi there, You've hit " + os.hostname() 
+      + ", on " + new Date +  "\n");
 };
 
 var www = http.createServer(handler);
@@ -14,4 +15,4 @@ var www = http.createServer(handler);
 var portNumber = 8080;
 www.listen(portNumber);
 
-console.log("Ying's web server listening on ..." + portNumber);
+console.log("Frank's web server listening on ..." + portNumber);
